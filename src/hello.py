74be488a1fb921fcc6f439b3283035303f08b0a1
Cyrus Ruheni <116971536+ruhenicyrus@users.noreply.github.com>
2026-01-04 03:03:35 +0300

    Initial commit - project structure, src, notebooks

---
+++ src/hello.py
@@ -0,0 +1,20 @@
+print("Hello World")
+print("Hello, Quant!")
+
+message = "We will build trading robots."
+print(message)
+
+# Basic data types
+an_integer = 2
+a_float = 2.5
+is_ready = True
+
+print(an_integer, a_float, is_ready)
+
+# Show types
+print(type(an_integer))
+print(type(a_float))
+print(type(is_ready))
+
+
+
